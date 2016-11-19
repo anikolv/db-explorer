36132b70e6128d6b3e66954481bfdacaa4570a9f
Alexander Nikolov <a.nikolov8@gmail.com>
2016-11-19 14:36:35 +0200

    Rename explorer folder to application

--- src/main/java/com/database/application/DbExplorerApplication.java
+++ src/main/java/com/database/application/DbExplorerApplication.java
@@ -1,4 +1,4 @@
-package com.database.explorer;
+package com.database.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
